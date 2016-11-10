4657b9c42d4e9fb71e4da283240b154dff603126
Lotos_ <zielonkamarcin00@gmail.com>
2016-11-11 00:05:08 +0100

    added about app fragment

---
+++ app/src/main/java/net/heliantum/ziedic/AboutAuthorFragment.java
@@ -0,0 +1,24 @@
+package net.heliantum.ziedic;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AboutAuthorFragment extends Fragment {
+
+
+    public AboutAuthorFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about_author, container, false);
+    }
+
+}
--- app/src/main/java/net/heliantum/ziedic/NavigationActivity.java
+++ app/src/main/java/net/heliantum/ziedic/NavigationActivity.java
@@ -1,12 +1,9 @@
 package net.heliantum.ziedic;
 
-import android.app.ActionBar;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -14,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class NavigationActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -74,8 +72,34 @@ public class NavigationActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.words) {
-            // Handle action
+        if (id == R.id.update_database) {
+            Toast.makeText(getApplicationContext(), "Obecnie niedostępne", Toast.LENGTH_SHORT).show();
+        }
+        else if(id == R.id.words) {
+            Toast.makeText(getApplicationContext(), "Obecnie niedostępne", Toast.LENGTH_SHORT).show();
+        }
+        else if(id == R.id.irregular_verbs) {
+            Toast.makeText(getApplicationContext(), "Obecnie niedostępne", Toast.LENGTH_SHORT).show();
+        }
+        else if(id == R.id.tenses) {
+            Toast.makeText(getApplicationContext(), "Obecnie niedostępne", Toast.LENGTH_SHORT).show();
+        }
+        else if(id == R.id.reported_speech) {
+            Toast.makeText(getApplicationContext(), "Obecnie niedostępne", Toast.LENGTH_SHORT).show();
+        }
+        else if(id == R.id.passive_voice) {
+            Toast.makeText(getApplicationContext(), "Obecnie niedostępne", Toast.LENGTH_SHORT).show();
+        }
+        else if(id == R.id.download_your_words) {
+            Toast.makeText(getApplicationContext(), "Obecnie niedostępne", Toast.LENGTH_SHORT).show();
+        }
+        else if(id == R.id.your_words) {
+            Toast.makeText(getApplicationContext(), "Obecnie niedostępne", Toast.LENGTH_SHORT).show();
+        }
+        else if(id == R.id.about_app) {
+            AboutAuthorFragment fragment = new AboutAuthorFragment();
+            FragmentManager manager = getSupportFragmentManager();
+            manager.beginTransaction().replace(R.id.layout_for_fragments, fragment).commit();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
