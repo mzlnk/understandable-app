053a61983e3741abc15f62f282ff33f741258066
Marcin Zielonka <zielonkamarcin00@gmail.com>
2017-08-12 22:48:24 +0200

    progress - added grammar modes support - part 4

--- app/src/main/java/pl/understandable/understandable_app/fragments/grammar/choice/GrammarChoiceModeFragment.java
+++ app/src/main/java/pl/understandable/understandable_app/fragments/grammar/choice/GrammarChoiceModeFragment.java
@@ -1,30 +1,202 @@
 package pl.understandable.understandable_app.fragments.grammar.choice;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import pl.understandable.understandable_app.R;
+import pl.understandable.understandable_app.data.entities_data.grammar.GrammarFillGapData;
+import pl.understandable.understandable_app.data.entities_data.grammar.GrammarQuizData;
+import pl.understandable.understandable_app.data.entities_data.grammar.GrammarSentenceData;
+import pl.understandable.understandable_app.data.entities_data.phrases.PhrasesListData;
+import pl.understandable.understandable_app.data.entities_data.phrases.PhrasesQuizData;
+import pl.understandable.understandable_app.data.entities_data.phrases.PhrasesRepetitionData;
+import pl.understandable.understandable_app.data.enums.grammar.GrammarLearningMode;
+import pl.understandable.understandable_app.data.enums.phrases.PhrasesLearningMode;
+import pl.understandable.understandable_app.data.params.GrammarDataParams;
+import pl.understandable.understandable_app.data.params.PhrasesDataParams;
+import pl.understandable.understandable_app.fragments.grammar.fill_gap.GrammarFillGapFragment;
+import pl.understandable.understandable_app.fragments.grammar.preview.GrammarSetPreviewFragment;
+import pl.understandable.understandable_app.fragments.grammar.quiz.GrammarQuizFragment;
+import pl.understandable.understandable_app.fragments.grammar.sentence.GrammarSentenceFragment;
+import pl.understandable.understandable_app.fragments.phrases.choice.PhrasesChoiceModeFragment;
+import pl.understandable.understandable_app.fragments.phrases.choice.PhrasesChoiceWayFragment;
+import pl.understandable.understandable_app.fragments.phrases.list.PhrasesListFragment;
+import pl.understandable.understandable_app.fragments.phrases.quiz.PhrasesQuizFragment;
+import pl.understandable.understandable_app.fragments.phrases.repetition.PhrasesRepetitionFragment;
+import pl.understandable.understandable_app.utils.FragmentUtil;
+import pl.understandable.understandable_app.utils.ThemeUtil;
+import pl.understandable.understandable_app.utils.buttons.grammar.GrammarModeButton;
+import pl.understandable.understandable_app.utils.buttons.phrases.PhrasesModeButton;
+import pl.understandable.understandable_app.utils.font.Font;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class GrammarChoiceModeFragment extends Fragment {
 
+    private static final String DATA_PARAM = "grammar.mode.dataParam";
+    private static final String ID_PARAM = "grammar.mode.idParam";
+    private static final String NAME_PARAM = "grammar.mode.nameParam";
+
+    private RelativeLayout mainLayout;
+    private TableLayout modesLayout;
+    private TextView title;
+    private Button submit, back;
+
+    private List<GrammarModeButton> modes = new ArrayList<>();
+    private GrammarDataParams dataParams;
+    private String id;
+    private String name;
 
     public GrammarChoiceModeFragment() {
         // Required empty public constructor
     }
 
+    public static GrammarChoiceModeFragment newInstance(String param, String id, String name) {
+        GrammarChoiceModeFragment fragment = new GrammarChoiceModeFragment();
+        Bundle args = new Bundle();
+        args.putString(DATA_PARAM, param);
+        args.putString(ID_PARAM, id);
+        args.putString(NAME_PARAM, name);
+        fragment.setArguments(args);
+        return fragment;
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.f_grammar_choice_mode, container, false);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if(getArguments() != null) {
+            dataParams = new GrammarDataParams().fromString(getArguments().getString(DATA_PARAM));
+            id = getArguments().getString(ID_PARAM);
+            name = getArguments().getString(NAME_PARAM);
+        }
+        if(dataParams == null) {
+            dataParams = new GrammarDataParams();
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the modesLayout for this fragment
+        View rootView = inflater.inflate(R.layout.f_grammar_choice_mode, container, false);
+        loadViewsFromXml(rootView);
+        prepareLayout();
+        addListeners();
+
+        return rootView;
+    }
+
+    private void loadViewsFromXml(View rootView) {
+        mainLayout = (RelativeLayout) rootView.findViewById(R.id.f_grammar_choice_mode);
+        modesLayout = (TableLayout) rootView.findViewById(R.id.f_grammar_choice_mode_names_layout);
+        title = (TextView) rootView.findViewById(R.id.f_grammar_choice_mode_title);
+        submit = (Button) rootView.findViewById(R.id.f_grammar_choice_mode_submit);
+        back = (Button) rootView.findViewById(R.id.f_grammar_choice_mode_back);
+    }
+
+    private void prepareLayout() {
+        setAnimation();
+        setFonts();
+        prepareButtons();
+        initModeButtons();
+        addModeButtonsToTable();
+    }
+
+    private void setAnimation() {
+        Animation anim = AnimationUtils.loadAnimation(getContext(), R.anim.fade01);
+        mainLayout.setAnimation(anim);
+    }
+
+    private void setFonts() {
+        title.setTypeface(Font.TYPEFACE_MONTSERRAT);
+        submit.setTypeface(Font.TYPEFACE_MONTSERRAT);
+        back.setTypeface(Font.TYPEFACE_MONTSERRAT);
+    }
+
+    private void prepareButtons() {
+        ThemeUtil themeUtil = new ThemeUtil(getContext());
+        if(themeUtil.isDefaultTheme()) {
+            back.setBackgroundResource(R.drawable.field_rounded_pink);
+            submit.setBackgroundResource(R.drawable.field_rounded_light_pink);
+        } else {
+            back.setBackgroundResource(R.drawable.field_rounded_gray);
+            submit.setBackgroundResource(R.drawable.field_rounded_light_gray);
+        }
+    }
+
+    private void initModeButtons() {
+        for(GrammarLearningMode mode : GrammarLearningMode.values()) {
+            modes.add(new GrammarModeButton(getContext(), dataParams, mode, modes));
+        }
+    }
+
+    private void addModeButtonsToTable() {
+        TableRow currentImageRow = new TableRow(getContext());
+        TableRow currentTextRow = new TableRow(getContext());
+
+        int x = 0;
+        for (GrammarModeButton modeButton : modes) {
+            currentImageRow.addView(modeButton.getImage());
+            currentTextRow.addView(modeButton.getText());
+            if (x == 3) {
+                modesLayout.addView(currentImageRow);
+                modesLayout.addView(currentTextRow);
+                currentImageRow = new TableRow(getContext());
+                currentTextRow = new TableRow(getContext());
+                x = 0;
+            } else {
+                x++;
+            }
+        }
+        if (x != 0) {
+            modesLayout.addView(currentImageRow);
+            modesLayout.addView(currentTextRow);
+        }
+    }
+
+    private void addListeners() {
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                GrammarSetPreviewFragment grammarSetPreviewFragment = GrammarSetPreviewFragment.newInstance(id, name);
+                FragmentManager manager = getFragmentManager();
+                manager.beginTransaction().replace(R.id.layout_for_fragments, grammarSetPreviewFragment, FragmentUtil.F_PHRASES_CHOICE_WAY).commit();
+            }
+        });
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                switch (dataParams.mode) {
+                    case QUIZ:
+                        GrammarQuizData.createQuizDataFromParams(dataParams);
+                        transaction.replace(R.id.layout_for_fragments, new GrammarQuizFragment(), FragmentUtil.F_PHRASES_QUIZ);
+                        break;
+                    case FILL_GAPS:
+                        GrammarFillGapData.createRepetitionDataFromParams(dataParams);
+                        transaction.replace(R.id.layout_for_fragments, new GrammarFillGapFragment());
+                        break;
+                    case SENTENCE:
+                        GrammarSentenceData.createSentenceDataFromParams(dataParams);
+                        transaction.replace(R.id.layout_for_fragments, new GrammarSentenceFragment());
+                        break;
+                }
+                transaction.commit();
+            }
+        });
     }
 
 }
--- app/src/main/java/pl/understandable/understandable_app/fragments/grammar/quiz/GrammarQuizFragment.java
+++ app/src/main/java/pl/understandable/understandable_app/fragments/grammar/quiz/GrammarQuizFragment.java
@@ -1,30 +1,279 @@
 package pl.understandable.understandable_app.fragments.grammar.quiz;
 
-
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import pl.understandable.understandable_app.R;
+import pl.understandable.understandable_app.data.entities_data.custom_words_data.CustomWordsQuizData;
+import pl.understandable.understandable_app.data.entities_data.grammar.GrammarQuizData;
+import pl.understandable.understandable_app.data.entities_data.phrases.PhrasesQuizData;
+import pl.understandable.understandable_app.database.entity.GrammarQuizEntity;
+import pl.understandable.understandable_app.database.entity.PhraseEntity;
+import pl.understandable.understandable_app.fragments.phrases.quiz.PhrasesQuizFragment;
+import pl.understandable.understandable_app.fragments.phrases.quiz.PhrasesQuizResultFragment;
+import pl.understandable.understandable_app.utils.FragmentUtil;
+import pl.understandable.understandable_app.utils.ThemeUtil;
+import pl.understandable.understandable_app.utils.font.Font;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class GrammarQuizFragment extends Fragment {
 
+    private static final int QUESTION_ANSWER_TIME_IN_MILLIS = 5000;
+    private static final Random r = new Random();
+
+    private GrammarQuizData quizData;
+
+    private LinearLayout questionArea;
+    private TextView question, questionNumber;
+    private Button[] answers = new Button[4];
+    private ProgressBar time;
+    private Button finish;
+
+    private int timeLeft = QUESTION_ANSWER_TIME_IN_MILLIS;
+    private int correctOption;
+    private int chosenOption = -1;
+    private boolean isAnswered = false, timesUp = false;
+
+    private GrammarQuizEntity correctWord;
+    private String[] incorrectWords = new String[3];
 
     public GrammarQuizFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        quizData = GrammarQuizData.getQuizData();
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.f_grammar_quiz, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.f_grammar_quiz, container, false);
+        loadViewsFromXml(rootView);
+        prepareLayout();
+        addListeners();
+        initTimer();
+
+        return rootView;
+    }
+
+    private void loadViewsFromXml(View rootView) {
+        questionArea = (LinearLayout) rootView.findViewById(R.id.f_grammar_quiz_question_area);
+        question = (TextView) rootView.findViewById(R.id.f_grammar_quiz_question);
+        questionNumber = (TextView) rootView.findViewById(R.id.f_grammar_quiz_question_number);
+        answers[0] = (Button) rootView.findViewById(R.id.f_grammar_quiz_option_0);
+        answers[1] = (Button) rootView.findViewById(R.id.f_grammar_quiz_option_1);
+        answers[2] = (Button) rootView.findViewById(R.id.f_grammar_quiz_option_2);
+        answers[3] = (Button) rootView.findViewById(R.id.f_grammar_quiz_option_3);
+        time = (ProgressBar) rootView.findViewById(R.id.f_grammar_quiz_time);
+        finish = (Button) rootView.findViewById(R.id.f_grammar_quiz_finish);
+    }
+
+    private void initTimer() {
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new GrammarQuizFragment.QuizTask(getActivity()), 0, 1);
+    }
+
+    private void prepareLayout() {
+        setAnimation();
+        setFonts();
+        prepareButtons();
+        prepareViews();
+        setAnswers();
+    }
+
+    private void prepareViews() {
+        questionNumber.setText(new String("Pytanie " + GrammarQuizData.getQuizData().currentQuestion));
+        time.setMax(QUESTION_ANSWER_TIME_IN_MILLIS);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            time.setProgressTintList(ColorStateList.valueOf(Color.rgb(0, 153, 255)));
+        }
+    }
+
+    private void addListeners() {
+        for(int i = 0; i < 4; i++) {
+            final int option = i;
+            answers[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if(!timesUp) {
+                        chosenOption = option;
+                        isAnswered = true;
+
+                        if(chosenOption == correctOption) {
+                            quizData.correctAnswer();
+                        } else {
+                            quizData.incorrectAnswer();
+                        }
+                        getActivity().runOnUiThread(new GrammarQuizFragment.ShowAnswerTask());
+                    }
+                }
+            });
+        }
+
+        finish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.layout_for_fragments, new GrammarQuizResultFragment(), FragmentUtil.F_PHRASES_QUIZ_RESULT).commit();
+            }
+        });
+    }
+
+    private void setAnswers() {
+        GrammarQuizData.getQuizData().nextQuestion();
+        correctOption = GrammarQuizData.Util.getRandomCorrectOption();
+        correctWord = quizData.currentWord;
+        incorrectWords = quizData.currentWord.getIncorrectAnswers();
+
+        question.setText(correctWord.getQuestion());
+        for(int i = 0, j = 0; i < 4; i++) {
+            if(i == correctOption) {
+                answers[i].setText(correctWord.getCorrectAnswer());
+                continue;
+            }
+            answers[i].setText(incorrectWords[j]);
+            j++;
+        }
+    }
+
+    private void setAnimation() {
+        Animation anim = AnimationUtils.loadAnimation(getContext(), R.anim.fade00);
+        questionArea.setAnimation(anim);
+    }
+
+    private void setFonts() {
+        question.setTypeface(Font.TYPEFACE_MONTSERRAT);
+        questionNumber.setTypeface(Font.TYPEFACE_MONTSERRAT);
+        answers[0].setTypeface(Font.TYPEFACE_MONTSERRAT);
+        answers[1].setTypeface(Font.TYPEFACE_MONTSERRAT);
+        answers[2].setTypeface(Font.TYPEFACE_MONTSERRAT);
+        answers[3].setTypeface(Font.TYPEFACE_MONTSERRAT);
+        finish.setTypeface(Font.TYPEFACE_MONTSERRAT);
+    }
+
+    private void prepareButtons() {
+        ThemeUtil themeUtil = new ThemeUtil(getContext());
+        if(themeUtil.isDefaultTheme()) {
+            for(Button answer : answers) {
+                answer.setBackgroundResource(R.drawable.field_rounded_light_light_gray);
+            }
+            finish.setBackgroundResource(R.drawable.field_rounded_light_pink);
+        } else {
+            for(Button answer : answers) {
+                answer.setBackgroundResource(R.drawable.field_rounded_gray);
+            }
+            finish.setBackgroundResource(R.drawable.field_rounded_light_gray);
+        }
+    }
+
+    private class QuizTask extends TimerTask {
+
+        private FragmentActivity activity;
+
+        public QuizTask(FragmentActivity activity) {
+            this.activity = activity;
+        }
+
+        @Override
+        public void run() {
+            if(timeLeft > 0) {
+                timeLeft--;
+                time.setProgress(timeLeft);
+            }
+            if(timeLeft <= 0 || isAnswered) {
+                timesUp = true;
+                if(!isAnswered) {
+                    quizData.incorrectAnswer();
+                    activity.runOnUiThread(new GrammarQuizFragment.ShowAnswerTask());
+                }
+                this.cancel();
+            }
+        }
+    }
+
+    private class ShowAnswerTask implements Runnable {
+
+        @Override
+        public void run() {
+
+            if(getActivity() != null) {
+                int showCorrectOptionDelay = 1000;
+                int hideIncorrectOptionsDelay = 1500;
+
+                if (isAnswered && chosenOption != -1) {
+                    answers[chosenOption].setBackgroundResource(R.drawable.field_rounded_red);
+                }
+                if (chosenOption == correctOption) {
+                    answers[chosenOption].setBackgroundResource(R.drawable.field_rounded_green);
+                    showCorrectOptionDelay = 0;
+                    hideIncorrectOptionsDelay = 500;
+                }
+
+                android.os.Handler handler = new android.os.Handler();
+
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if(getContext() == null) {
+                            return;
+                        }
+                        answers[correctOption].setBackgroundResource(R.drawable.field_rounded_green);
+                    }
+                }, showCorrectOptionDelay);
+
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if(getContext() == null) {
+                            return;
+                        }
+                        for (int i = 0; i < 4; i++) {
+                            if (i == correctOption) {
+                                continue;
+                            }
+                            Animation anim = AnimationUtils.loadAnimation(getContext(), R.anim.fade_out);
+                            answers[i].startAnimation(anim);
+                        }
+
+                    }
+                }, hideIncorrectOptionsDelay);
+
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if(getActivity() == null) {
+                            return;
+                        }
+                        FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                        if (quizData.wordsLeft.size() > 1) {
+                            transaction.replace(R.id.layout_for_fragments, new GrammarQuizFragment(), FragmentUtil.F_PHRASES_QUIZ).commit();
+                        } else {
+                            transaction.replace(R.id.layout_for_fragments, new GrammarQuizResultFragment(), FragmentUtil.F_PHRASES_QUIZ_RESULT).commit();
+                        }
+                    }
+                }, 3000);
+            }
+        }
     }
 
 }
