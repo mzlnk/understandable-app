f36acc4871d66afda3c5bf3405f8d3638df6a5cc
Marcin Zielonka <zielonkamarcin00@gmail.com>
2017-08-14 17:41:08 +0200

    refactor - edit texts

--- app/src/main/java/pl/understandable/understandable_app/fragments/custom_words/spelling/CustomWordsSpellingFragment.java
+++ app/src/main/java/pl/understandable/understandable_app/fragments/custom_words/spelling/CustomWordsSpellingFragment.java
@@ -1,5 +1,6 @@
 package pl.understandable.understandable_app.fragments.custom_words.spelling;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.Typeface;
@@ -11,11 +12,14 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
